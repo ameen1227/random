66ab79a8709289a6362919b3b00bd64c408b1cff
ameen1227 <ameenhussain2005@gmail.com>
2025-10-15 22:02:29 +0530

    Create jokke.py

---
+++ jokke.py
@@ -0,0 +1,10 @@
+import random
+
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "I told my computer I needed a break, and now it won’t stop sending me KitKat ads.",
+    "Why do programmers prefer dark mode? Because light attracts bugs."
+]
+
+print("Here's a random joke for you:")
+print(random.choice(jokes))
